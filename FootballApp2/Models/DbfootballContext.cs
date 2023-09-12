ef211baef53cb2fbf83235e1160b26d2757bbc81
qwasderr <maxmarkov02@gmail.com>
2023-09-13 00:14:04 +0300

    App push

--- FootballApp2/Models/DbfootballContext.cs
+++ FootballApp2/Models/DbfootballContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 
 namespace FootballApp2.Models;
@@ -21,9 +22,10 @@ public partial class DbfootballContext : DbContext
 
     public virtual DbSet<Team> Teams { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server= DESKTOP-JG8P8MC\\SQLEXPRESS;\nDatabase=DBFootball; Trusted_Connection=True; TrustServerCertificate=True");
+    //=> optionsBuilder.UseSqlServer("Server= DESKTOP-JG8P8MC\\SQLEXPRESS;\nDatabase=DBFootball; Trusted_Connection=True; TrustServerCertificate=True");
+      => optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"))*/
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
