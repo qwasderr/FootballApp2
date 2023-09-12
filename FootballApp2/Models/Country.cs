2462eadf18b554843be5c5dc8366bd71a48665f6
Max <Max@DESKTOP-JG8P8MC>
2023-09-12 21:19:25 +0300

    Add project files.

---
+++ FootballApp2/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballApp2.Models;
+
+public partial class Country
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public float Rating { get; set; }
+
+    public virtual ICollection<Team> Teams { get; set; } = new List<Team>();
+}
---
+++ FootballApp2/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballApp2.Models;
+
+public partial class Team
+{
+    public int Id { get; set; }
+
+    public string DivName { get; set; } = null!;
+
+    public int Division { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int CountryId { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual Country Country { get; set; } = null!;
+
+    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+}
