53a1846b0584a444510638b5633c3919286256d0
Max Markov <maxmarkov02@knu.ua>
2023-09-27 23:44:38 +0300

    SEO optimization

--- FootballApp2/Program.cs
+++ FootballApp2/Program.cs
@@ -83,6 +83,6 @@ app.MapControllerRoute(
    name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
-app.UseRewriter(new RewriteOptions().AddRedirectToWwwPermanent().AddRedirectToHttpsPermanent());
+//app.UseRewriter(new RewriteOptions().AddRedirectToWwwPermanent().AddRedirectToHttpsPermanent());
 app.Run();
 public partial class Program { }
